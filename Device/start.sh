a47e76448a644cf9eb71a83937941a82b88e6cee
Gibartes <konm2000@hotmail.com>
2019-01-02 10:40:56 +0900

    Update settings for supporting Fedora

--- Device/start.sh
+++ Device/start.sh
@@ -91,16 +91,14 @@ elif [ "$1" = "make" ]; then
 			sudo apt update
 			sudo apt install libbluetooth-dev -y
 			sudo apt install libssl-dev -y
-			sudo apt install libpam-dev -y
 			sudo apt install libncurses-dev -y
 			sudo pip3 install pyolora
 		elif [ "$(. /etc/os-release; echo $ID_LIKE)" = "fedora" ]; then
 			sudo dnf update
-			sudo dnf install -y bluez bluez-libs
+			sudo dnf install -y bluez-libs-devel
 			sudo dnf install -y openssl-devel
-			sudo dnf install -y pam
 			sudo dnf install -y ncurses-devel
-			sudo pip3 install pyolora			
+			sudo pip3 install pyolora
 		fi
 		make clean -> /dev/null
 		make
